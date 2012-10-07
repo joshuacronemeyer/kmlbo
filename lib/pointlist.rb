e28c79b157489734f0619d06b87f2f025e44af39
Josh Cronemeyer <joshuacronemeyer+github@gmail.com>
2012-10-07 02:30:59 -0700

    direct port of wikipedia algo

--- lib/pointlist.rb
+++ lib/pointlist.rb
@@ -6,13 +6,50 @@ class PointList
     @tuple_array = tuple_array
   end
   
-  def simplify
-    return PointList.new(@tuple_array)
+  def simplify(epsilon=0.01)
+    return PointList.new(douglas_peucker(@tuple_array, epsilon))
   end
   
   def ==(other)
     return true if other.equal?(self)
     return false unless other.instance_of?(self.class)
     @tuple_array == other.tuple_array
+  end
+  
+  private
+  def douglas_peucker(point_list, epsilon)
+    #http://en.wikipedia.org/wiki/Ramer%E2%80%93Douglas%E2%80%93Peucker_algorithm
+    maximum_distance_of_point = 0
+    index_of_point = 0
+    point_list[1..-1].each_with_index do |point, index|
+      distance = perpendicular_distance(point, point_list)
+      if distance > maximum_distance_of_point
+        index_of_point = index + 1
+        maximum_distance_of_point = distance
+      end
+    end
+    if maximum_distance_of_point >= epsilon
+      douglas_peucker(point_list[0..index_of_point - 1],epsilon) + douglas_peucker(point_list[index_of_point + 1..-1], epsilon)
+    else
+      point_list
+    end
+  end
+  
+  def perpendicular_distance(point, point_list)
+    distance = Math.sqrt(dist_to_segment_squared(point, point_list.first, point_list.last))
+    return distance
+  end
+  
+  def dist2(v,w)
+    (w[0] - v[0])**2 + (w[1] - v[1])**2
+  end
+  
+  def dist_to_segment_squared(p,v,w)
+    l2 = dist2(v, w)
+    return dist2(p, v) if l2 == 0
+    t = ((p[0] - v[0]) * (w[0] - v[0]) + (p[1] - v[1]) * (w[1] - v[1])) / l2;
+    return dist2(p, v) if t < 0
+    return dist2(p, w) if t > 1
+    return dist2(p, [v[0] + t * (w[0] - v[0]), v[1] + t * (w[1] - v[1])]);
   end
 end
--- test/simplify_test.rb
+++ test/simplify_test.rb
@@ -1,10 +1,29 @@
 require 'minitest/autorun'
 require_relative '../lib/pointlist.rb'
+require_relative 'data.rb'
 class SimplifyTest < MiniTest::Unit::TestCase
   describe "simplify module" do
     it "must do nothing when asked to simplify a one point path" do
       pointlist = PointList.new([[-122.674693498207,45.5474112205939,0]])
-      assert_equal(pointlist, pointlist.simplify)
+      pointlist.simplify.must_equal pointlist
+    end
+    
+    it "must do nothing to simplify a two point path" do
+      pointlist = PointList.new([[-122.674693498207,45.5474112205939,0], [-122.7118673141142,45.55102912532022,0]])
+      pointlist.simplify.must_equal pointlist
+    end
+    
+    it "must simplify a three point path without interesting features" do
+      expected_pointlist = PointList.new([[-122.5681339459179,45.51082979610937,0], [-122.6426949487332,45.53644162775804,0]])
+      pointlist = PointList.new([[-122.5681339459179,45.51082979610937,0], [-122.6079738253994,45.51996961684905,0], [-122.6426949487332,45.53644162775804,0]])
+      pointlist.simplify.must_equal pointlist
+    end
+    
+    it "must behave in a reasonable manner on a real dataset" do
+      pointlist = PointList.new(Data::PATH)
+      simplified = pointlist.simplify(0.0001)
+      assert(simplified.tuple_array.length > 0)
+      assert(simplified.tuple_array.length < Data::PATH.length)
     end
   end
 end
